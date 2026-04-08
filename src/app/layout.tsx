b99fea3fdb4b4e04a98f686421aa0f99db5e4bee
OwlDane <zidunnoaoe@gmail.com>
2026-04-09 02:02:17 +0700

    style(layout): add smooth scroll behavior to root HTML element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
   return (
     <html
       lang="id"
+      data-scroll-behavior="smooth"
       className={`${inter.variable} ${plusJakarta.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
